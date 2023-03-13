a19b1372eb98538bbf1f18106be127d0818bd759
Юсубов Юсуб Махмуд <Usub_jg9z@itstep.edu.az>
2023-03-13 16:32:28 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+class dog:
+    def __init__(self):
+        self.height = 20
+        self.weight = 12
+        self.age = 4
+        print(self.age)
+        print(self.weight)
+        print(self.height)
+
+Popit = dog()
